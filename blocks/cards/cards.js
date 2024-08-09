bc781c751e86af56daf400d55fe4a23df9de1f72
Kiagias <kkiagias@deloitte.gr>
2024-08-09 17:57:55 +0300

    Card class contained

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -6,6 +6,7 @@ export default function decorate(block) {
   const ul = document.createElement('ul');
   [...block.children].forEach((row) => {
     const li = document.createElement('li');
+    li.classList.add('contained');
     moveInstrumentation(row, li);
     while (row.firstElementChild) li.append(row.firstElementChild);
     [...li.children].forEach((div) => {
