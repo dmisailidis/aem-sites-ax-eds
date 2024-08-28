1f8070fe762286812645b2624a64ed2616d44d47
Kiagias <kkiagias@deloitte.gr>
2024-08-28 13:32:43 +0300

    Card default style

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -6,12 +6,14 @@ export default function decorate(block) {
   const ul = document.createElement('ul');
   [...block.children].forEach((row) => {
     const li = document.createElement('li');
-    li.classList.add('contained');
     moveInstrumentation(row, li);
     while (row.firstElementChild) li.append(row.firstElementChild);
     [...li.children].forEach((div) => {
       if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
       else div.className = 'cards-card-body';
+    });
+    li.querySelectorAll('a').forEach((a) => {
+      a.classList.add('icon-right-dir'); 
     });
     ul.append(li);
   });
