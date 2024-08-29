864e27ef1565707cc54b6a1be6f39367d8aa9e3b
dmisailidis <dmisailidis@deloitte.gr>
2024-08-29 15:55:29 +0300

    Add icon options for accordion

--- blocks/accordion/accordion.js
+++ blocks/accordion/accordion.js
@@ -1,35 +1,36 @@
 function hasWrapper(el) {
   return !!el.firstElementChild && window.getComputedStyle(el.firstElementChild).display === 'block';
 }
-function toggleAccordion(navToggle) {
+function toggleAccordion(navToggle, menuTitle) {
   navToggle.open = !navToggle.open;
+  const ariaExpanded = navToggle.open ? 'true' : 'false';
+  menuTitle.setAttribute('aria-expanded', ariaExpanded);
+  menuTitle.classList.toggle('icon-down-open-big', !navToggle.open);
+  menuTitle.classList.toggle('icon-up-open-big', navToggle.open);
 }
 export default function decorate(block) {
   [...block.children].forEach((row) => {
-    // decorate accordion item label
     const label = row.children[0];
     const summary = document.createElement('summary');
     summary.className = 'accordion-item-label';
     summary.append(...label.childNodes);
     if (!hasWrapper(summary)) {
       summary.innerHTML = `
-        <h3>${summary.innerHTML}</h3>
+        <h3>${summary.innerText}</h3>
      `;
     }
 
     const buttonWithIcon = document.createElement('button');
     buttonWithIcon.classList.add('accordion-button');
     buttonWithIcon.setAttribute('aria-expanded', 'false');
-    buttonWithIcon.innerHTML = '<span class="plus-icon" aria-expanded="false"></span>';
+    buttonWithIcon.classList.add('icon-down-open-big');
     summary.appendChild(buttonWithIcon);
 
-    // decorate accordion item body
     const body = row.children[1];
     body.className = 'accordion-item-body';
     if (!hasWrapper(body)) {
       body.innerHTML = `<p>${body.innerHTML}</p>`;
     }
-    // decorate accordion item
     const details = document.createElement('details');
     details.className = 'accordion-item';
     details.append(summary, body);
@@ -38,9 +39,9 @@ export default function decorate(block) {
     row.append(details);
 
     const menuTitle = summary.querySelector('button');
-
-    menuTitle.addEventListener('click', () => {
-      toggleAccordion(details);
+    summary.addEventListener('click', (e) => {
+      e.preventDefault();
+      toggleAccordion(details, menuTitle);
     });
   });
 }
