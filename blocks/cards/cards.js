2eda008c8eb53d1e5bb231153031ec62114fcd0c
Kiagias <kkiagias@deloitte.gr>
2024-09-06 14:38:02 +0300

    New component definition

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -4,7 +4,9 @@ import { moveInstrumentation } from '../../scripts/scripts.js';
 export default function decorate(block) {
   /* change to ul, li */
   const ul = document.createElement('ul');
+  console.log(block);
   [...block.children].forEach((row) => {
+    console.log(row);
     const li = document.createElement('li');
     moveInstrumentation(row, li);
     while (row.firstElementChild) li.append(row.firstElementChild);
