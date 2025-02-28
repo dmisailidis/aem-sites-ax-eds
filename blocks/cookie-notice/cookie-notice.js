b17b593cb386dd28cba09bd789068de662f74e17
mguerriero <mguerriero@deloitte.it>
2025-02-28 11:12:48 +0100

    add cookie-notice block

---
+++ blocks/cookie-notice/cookie-notice.js
@@ -0,0 +1,30 @@
+export default function decorate(block) {
+  /*
+  const text = document.createElement('p');
+  text.appendChild(document.createTextNode('This is a cookie notice.'));
+  const btn = document.createElement('button');
+  btn.setAttribute('id', 'accept-cookies');
+  btn.appendChild(document.createTextNode('Accept'));
+  block.replaceChildren();
+  block.appendChild(text);
+  block.appendChild(btn);
+
+  initCookieNotice();
+  */
+}
+
+function initCookieNotice() {
+    const cookieNotice = document.querySelector('.cookie-notice.block');
+    const acceptCookiesButton = document.getElementById('accept-cookies');
+
+    // Check if the user has already accepted cookies
+    if (!localStorage.getItem('cookiesAccepted') || localStorage.getItem('cookiesAccepted') !== 'true') {
+        cookieNotice.style.display = 'block';
+    }
+
+    // Handle the accept button click
+    acceptCookiesButton.addEventListener('click', function () {
+        localStorage.setItem('cookiesAccepted', 'true');
+        cookieNotice.style.display = 'none';
+    });
+};
