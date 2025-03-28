bd1776c49005e80a49f4ccaa8753fe8c9bc72885
mguerriero <mguerriero@deloitte.it>
2025-03-28 12:00:58 +0100

    update pdf viewer block

--- blocks/pdf-viewer/pdf-viewer.js
+++ blocks/pdf-viewer/pdf-viewer.js
@@ -6,7 +6,25 @@ const loadScript = () => {
   return script;
 };
 
-export default function decorate(block) {
+const loadClientId = async () => {
+  try {
+    const response = await fetch('http://localhost:3001/pdf-client-id');
+    if (!response.ok) {
+      throw new Error(`Failed to fetch Client Id: ${response.status}`);
+    }
+
+    const data = await response.json();
+    if (!data.clientId) {
+      throw new Error('Client Id not found in response');
+    }
+
+    return data.clientId;
+  } catch (error) {
+    console.error('PDF Viewer: Error initializing pdf viewer', error);
+  }
+};
+
+export default async function decorate(block) {
   const [fileDiv, embedDiv] = [...block.children];
   const filePathContainer = fileDiv.querySelector('p');
   const filePath = filePathContainer ? filePathContainer.innerText : null;
@@ -15,6 +33,7 @@ export default function decorate(block) {
 
   if (filePath) {
     loadScript();
+    const clientId = await loadClientId();
 
     const pdfViewer = document.createElement('div');
     pdfViewer.id = "pdf-viewer";
@@ -27,7 +46,7 @@ export default function decorate(block) {
 
     document.addEventListener("adobe_dc_view_sdk.ready", function() {
       var adobeDCView = new AdobeDC.View({
-          clientId: "30dc4a499b344bb08b65c0565d550089",
+          clientId: clientId,
           divId: "pdf-viewer"
       });
       adobeDCView.previewFile({
