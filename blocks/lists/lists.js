497e13c40f9d2a3f1aa4d0f2ac5b9b0ba737031d
cdicillo <cdicillo@deloitte.it>
2025-03-31 17:20:34 +0200

    update lists block

--- blocks/lists/lists.js
+++ blocks/lists/lists.js
@@ -2,8 +2,13 @@ export default function decorate(block) {
 
     const items = [...block.children]; //get all children of the block
 
-    const select = document.querySelector('[name = "orderBy"]'); //get the select element
-    if(!select) return; //if select element is not found, exit
+    const tagOrder = document.createElement('div'); //create a div to hold the tag order
+    tagOrder.classList.add('orderBy'); //add class to the div
+
+    const container = document.createElement('div');
+    container.classList.add('lists-container');
+
+    const select = document.createElement('select');
 
     //order by title or description
     function sortItems(sortKey) {
@@ -16,10 +21,27 @@ export default function decorate(block) {
         items.forEach(item => block.appendChild(item)); //append sorted items to the block
     }
 
+    block.innerHTML = '';
+    block.appendChild(select);
+
+    const ul = document.createElement('ul');
+    ul.classList.add('lists-items');
+    container.appendChild(ul);
+
     select.addEventListener('change', (e) => {
         sortItems(e.target.value.toLowerCase());
     });
 
     sortItems(select.value.toLowerCase());
 
+    items.forEach(item => {
+        const li = document.createElement('li');
+        li.classList.add('list-item');
+        li.appendChild(item);
+        ul.appendChild(li);
+    });
+
+    block.appendChild(container);
+
 }
+
