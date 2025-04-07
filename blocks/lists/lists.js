23bf4e900cd7ad7a9815f0814ffcb977042e62a0
cdicillo <cdicillo@deloitte.it>
2025-04-07 14:13:14 +0200

    update lists block

--- blocks/lists/lists.js
+++ blocks/lists/lists.js
@@ -1,50 +1,3 @@
-/*
-export default function decorate(block) {
-  // Crea l'elemento lista principale
-  const list = document.createElement('ul');
-  list.classList.add('list');
-
-  [...block.children].forEach((row) => {
-    // Estrai titolo e descrizione dalle colonne della riga
-    const [title, description, link, icon] = row.children;
-    console.log(`Title: ${title}, Description: ${description}`);
-
-    */
-/*//*
-/ Crea l'elemento della lista
-    const listItem = document.createElement('li');
-    listItem.classList.add('list-item');
-
-    // Crea titolo
-    if (title) {
-      const titleElement = document.createElement('div');
-      titleElement.classList.add('list-item-title');
-      titleElement.textContent = title;
-      listItem.appendChild(titleElement);
-    }
-
-    // Crea descrizione
-    if (description) {
-      const descElement = document.createElement('div');
-      descElement.classList.add('list-item-description');
-      descElement.textContent = description;
-      listItem.appendChild(descElement);
-    }
-
-    // Aggiungi l'elemento alla lista
-    list.appendChild(listItem);*//*
-
-  });
-
-  // Sostituisci il contenuto del blocco con la lista creata
-*/
-/*  block.textContent = '';
-  block.appendChild(list);*//*
-
-}
-*/
-
-
 export default function decorate(block) {
     // remove empty divs
     const items = [...block.children].filter((item) => {
@@ -57,7 +10,8 @@ export default function decorate(block) {
         }
         return true;
     });
-    console.log("orderBy: ",orderBy)
+
+
     const list = document.createElement('ul');
     list.className = 'list';
     const addedItems = new Set(); // keep track of added items
