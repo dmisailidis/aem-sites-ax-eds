504021e742d3a98e37e9b9c54a1004fc15f837c2
vasilisrousis1 <varousis@deloitte.gr>
2025-05-02 13:11:12 +0300

    updated image-link

--- blocks/image-link/image-link.js
+++ blocks/image-link/image-link.js
@@ -10,10 +10,32 @@ export default function decorate(block) {
     if (img) {
       img.setAttribute('alt', altText);
     }
+
+    // Create new anchor but preserve data attributes
     const newAnchor = document.createElement('a');
     newAnchor.setAttribute('href', url);
+
+    // Copy any data-aue-* attributes from the original anchor
+    Array.from(anchor.attributes)
+      .filter((attr) => attr.name.startsWith('data-aue-'))
+      .forEach((attr) => {
+        newAnchor.setAttribute(attr.name, attr.value);
+      });
+
+    // Preserve data attributes on the picture element
+    Array.from(picture.attributes)
+      .filter((attr) => attr.name.startsWith('data-aue-'))
+      .forEach((attr) => {
+        picture.setAttribute(attr.name, attr.value);
+      });
+
     newAnchor.appendChild(picture);
-    block.innerHTML = '';
+
+    // Instead of clearing innerHTML, remove children individually
+    while (block.firstChild) {
+      block.removeChild(block.firstChild);
+    }
+
     block.appendChild(newAnchor);
   }
 }
